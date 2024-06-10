d761ead29b9f5abdd3eda823aa049a481748f0ee
Nyadej <naomiadejumo@gmail.com>
2024-06-10 16:00:49 +0100

    Added search functionality using API

--- index.js
+++ index.js
@@ -1,9 +1,28 @@
+function refreshWeather(response) {
+    let temperatureElement = document.querySelector("#temperature");
+    let temperature = response.data.temperature.current;
+    let cityElement = document.querySelector("#city");
+
+    cityElement.innerHTML = response.data.city;
+    temperatureElement.innerHTML = response.data.temperature.current;
+}
+
+function searchCity(city) {
+    let apiKey = "eb4bte9c7c6fffca96f39fo1e0c09d1c";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(refreshWeather);
+}
+
+
 function handleSearchSubmit(event) {
     event.preventDefault();
     let searchInput = document.querySelector("#search-form-input");
     let cityElement = document.querySelector("#city");
     cityElement.innerHTML = searchInput.value;
+    searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("London");
